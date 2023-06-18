ac3f8936ee813dc6c699edf330e9f6d6305a8f82
George Jiadze <georgejiadze@gmail.com>
2023-06-19 00:44:13 +0300

    account debug

--- app/src/main/java/com/example/cinticket/entities/Account.kt
+++ app/src/main/java/com/example/cinticket/entities/Account.kt
@@ -1,5 +1,26 @@
 package com.example.cinticket.entities
 
-class AccountTest {
+import com.example.cinticket.accounts.entities.AccountDbEntity
 
+data class Account(
+    val accountId: Long,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val password: String,
+    var cardNumber: String,
+    var dateOfCard:String,
+    var CVV_code:Long
+)
+{
+    fun toAccountDbEntity(): AccountDbEntity = AccountDbEntity(
+        accountId = accountId,
+        firstName = firstName,
+        lastName = lastName,
+        email = email,
+        password = password,
+        cardNumber = cardNumber,
+        dateOfCard=dateOfCard,
+        CVV_code = CVV_code
+    )
 }
